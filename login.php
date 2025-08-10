efd02a2e4c1aceca497840e44d05d2be3f230b72
Jebreil Jhon Blancada <jebreilblancada20@gmail.com>
2025-08-11 01:40:24 +0800

    setup login

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+  <div class="content">
+    <div class="box">
+      <p class="Login">Login</h1>
+
+      <form action="" method="post">
+        <p>Email</p>
+        <input id="email" name="email" class="search-bar" type="email" placeholder="Enter Email" required>
+        <p>Password</p>
+        <input id="password" name="password" class="search-bar" type="password" placeholder="Enter Password" required> <br>
+        <button class="loginbutton">Login</button>
+        <p class="signup">
+          Don't have an account? <a href="" class="under">Sign Up</span>
+        </p>
+    </div>
+  </div>
+  </form>
