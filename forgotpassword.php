89d0611656e41f4ea4fd7ea16d6ecb449325a8d5
Christian Jay Barcos <cbarcos111@gmail.com>
2025-08-14 18:23:24 +0800

    created forgotpassword page

---
+++ forgotpassword.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORGOT PASSWORD</title>
+    <link rel="stylesheet" href="../css/pass.css">
+</head>
+<body>
+    <div class="Forgot">
+        <h1 class="fpass">FORGOT  PASSWORD</h1>
+        <p>Enter your email address</p>
+        <div class="labil"> <input type="email" placeholder="Enter your email address"></div>
+        <button class="Continue"><strong><a href="../ForgotPass/Newpass.html">Continue</a> </strong></button>
+    </div>
+</body>
+</html>
---
+++ newpass.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NEW PASSWORD</title>
+    <link rel="stylesheet" href="../css/pass.css">
+</head>
+<body>
+    <div class="New">
+        <h1 class="npass">NEW PASSWORD</h1>
+        <p>Please create new password that yung hindi mo makalilimutan plsss</p>
+        <div class="Create"> <input type="password" placeholder="Create New Password"></div>
+        <div class="Confirm"> <input type="password" placeholder="Confirm Your New Password"></div>
+        <button class="Change"> <strong>Change</strong> </button>
+    </div>
+</body>
+</html>
