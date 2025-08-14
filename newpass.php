66e5595e3f4cea7792d854ddbf00ed2a980815b1
Christian Jay Barcos <cbarcos111@gmail.com>
2025-08-14 18:26:37 +0800

    #

--- newpass.php
+++ newpass.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NEW PASSWORD</title>
-    <link rel="stylesheet" href="../css/pass.css">
+    <link rel="stylesheet" href="../css/forgotpass.css">
 </head>
 <body>
     <div class="New">
