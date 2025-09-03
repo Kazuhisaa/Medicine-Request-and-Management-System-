56ed990b97e410e868bb0945d2f381fbde5e363c
Jebreil Jhon Blancada <jebreilblancada20@gmail.com>
2025-09-03 16:24:55 +0800

    fixed timezones

--- config/db.php
+++ config/db.php
@@ -4,7 +4,6 @@ $user = "root";
 $pass = "";
 $db   = "medrms";
 
-mysqli_query($conn, "SET time_zone = '+00:00'");
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
@@ -12,3 +11,4 @@ $conn = mysqli_connect($host, $user, $pass, $db);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+mysqli_query($conn, "SET time_zone = '+00:00'");
